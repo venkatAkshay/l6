c171831daa434e03b04fc77de68f66a720a84d37
venkatAkshay <113366014+venkatAkshay@users.noreply.github.com>
2022-12-10 20:51:11 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
     markAsCompleted() {
       return this.update({ completed: true });
     }
+    static getTodo() {
+      return this.update({ order : [["id", "ASC"]] });
+    }
   }
   Todo.init(
     {
