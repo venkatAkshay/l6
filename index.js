d856030ac0bf970b20847dcb6a6bdc990dcb001f
venkatAkshay <113366014+venkatAkshay@users.noreply.github.com>
2022-12-12 20:41:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Started express server at port 3000");
+app.listen(port, () => {
+  console.log(`Started express server at port 3000- ${port}`);
 });
